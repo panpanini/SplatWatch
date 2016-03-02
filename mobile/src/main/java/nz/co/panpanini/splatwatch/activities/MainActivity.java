6f1639e110234720e1b9b1ec18cc94a084902582
panpanini <zell1990@gmail.com>
2016-03-02 21:54:17 +1300

    Add toolbar to MainActivity

--- mobile/src/main/java/nz/co/panpanini/splatwatch/activities/MainActivity.java
+++ mobile/src/main/java/nz/co/panpanini/splatwatch/activities/MainActivity.java
@@ -1,16 +1,30 @@
 package nz.co.panpanini.splatwatch.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import nz.co.panpanini.splatwatch.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        // set the toolbar to be slightly higher than the content
+        ViewCompat.setElevation(toolbar, 5f);
+
 
     }
 
